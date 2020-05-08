07d3fc37b50e0d89752c9a3e6b5be25e1d19d178
Rungsikorn R <rungsikorn.r@kbtg.tech>
2020-05-08 19:43:22 +0700

    add todo mvc file unimplemented

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,7 +4,12 @@ import * as React from 'react'
 export default () => {
     return (
         <div>
-            Hello, World
+            <section className="todoapp">
+                <header className="header">
+                <h1>todos</h1>
+				    <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+                </header>
+            </section>
         </div>
     )
 }
